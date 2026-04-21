83abc6c7f5b3a530c235ccffeaf45d9fa18a631f
Justine <t.nichole.2807@gmail.com>
2026-04-21 16:46:57 -0600

    debugging JS

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 function generatePoem(event) {
   event.preventDefault();
 
-  alert("Generating your poem...");
+ let apiKey="7da4d2833b785tc382cf9d899bo46033";
+ let prompt = "Generate a poem";
+ let context ="You are a master poet. You can write poems on any topic. Write in basic HTML format. Do not include any explanations or additional text. Just write the poem.";
+ let apiUrl =
+  `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(generatePoem); {
+  };
 }
 
 let poemFormElement = document.querySelector("#poem-form");
